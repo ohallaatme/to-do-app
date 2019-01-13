017c3355bddc577aaf78be6fb5e79674ac236ca0
Katie <kohalloran1215@gmail.com>
2019-01-12 19:20:22 -0600

    Checkpoint: React To-Do: State and Props Completed

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -6,7 +6,10 @@ class ToDo extends Component {
 //step 3 - define a render() method on your class. This Method is required
   render() {
     return (
-      <li> A todo will go here </li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+      </li>
     );
   }
 }
